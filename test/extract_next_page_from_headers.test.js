53fc339cc4eadb0aee3b6cd81fd378957d62c410
nelsonic <contact.nelsonic+github@gmail.com>
2017-01-16 23:48:45 +0000

    adds simple regex to extract urls from rawHeaders for #7

--- test/extract_next_page_from_headers.test.js
+++ test/extract_next_page_from_headers.test.js
@@ -7,3 +7,13 @@ var fixture = fs.readFileSync(path.resolve(CWD, file), 'utf8');
 console.log(fixture);
 var headers = fixture.split('\n');
 console.log(headers.length);
+
+var found = headers.filter(function (line) {
+  return line.indexOf('api.github') > -1;
+})
+// [ '\'<https://api.github.com/organizations/11708465/repos?page=2>; rel="next", <https://api.github.com/organizations/11708465/repos?page=7>; rel="last"\',' ]
+console.log(found[0]);
+var re = /<(.*?)>/g;
+
+var one = found[0].match(re);
+console.log(one);
