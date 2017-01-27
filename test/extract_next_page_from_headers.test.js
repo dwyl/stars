a28c38d675cd28b9a2e3568bb9ccd7405543450e
nelsonic <contact.nelsonic+github@gmail.com>
2017-01-27 21:40:30 +0000

    adds initial implementation of next link extractor https://github.com/dwyl/stars/issues/12

--- test/extract_next_page_from_headers.test.js
+++ test/extract_next_page_from_headers.test.js
@@ -2,18 +2,23 @@ var fs = require('fs');
 var path = require('path');
 var file = 'test/fixtures/dwyl_repos_rawheaders.txt';
 var CWD = process.cwd();
-// console.log('CWD:', CWD)
-var fixture = fs.readFileSync(path.resolve(CWD, file), 'utf8');
-console.log(fixture);
-var headers = fixture.split('\n');
-console.log(headers.length);
 
-var found = headers.filter(function (line) {
-  return line.indexOf('api.github') > -1;
-})
-// [ '\'<https://api.github.com/organizations/11708465/repos?page=2>; rel="next", <https://api.github.com/organizations/11708465/repos?page=7>; rel="last"\',' ]
-console.log(found[0]);
-var re = /<(.*?)>/g;
+function extract (filepath) {
+  var next;
+  var fixture = fs.readFileSync(path.resolve(CWD, filepath), 'utf8');
+  var re = /<(.*?)>/g;
+  // console.log(fixture);
+  var headers = fixture.split('\n');
+  var found = headers.filter(function (line) {
+    return line.indexOf('api.github') > -1;
+  })
+  if(found.length > 0) {
+    next = found[0].match(re)[0].replace('<', '').replace('>', '');
+  }
+  return next;
+}
 
-var one = found[0].match(re);
-console.log(one);
+console.log(extract(file))
+
+var file2 = 'test/fixtures/namegen_repos_rawheaders_no_next.txt';
+console.log(extract(file2))
