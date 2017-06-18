6fd715f94e7b14880ae3e0ac2b6eb58d473a163a
nelsonic <contact.nelsonic+github@gmail.com>
2017-06-19 00:07:39 +0100

    update faces script to source images form GitHub CDN so it will work online closes https://github.com/dwyl/stars/issues/18

--- data/download_avatars.js
+++ data/download_avatars.js
@@ -51,9 +51,6 @@ var request = require('request');
 
 function download (uri, filename, callback){
   request.head(uri, function(err, res, body){
-    // console.log('content-type:', res.headers['content-type']);
-    // console.log('content-length:', res.headers['content-length']);
-
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
